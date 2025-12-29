c562369de1bbb81527968f404d17f974797bd581
Mickel Arroz <arrozmickel@gmail.com>
2025-12-29 18:34:57 -0400

    6B Store con estado complejo

--- notes-redux/src/App.jsx
+++ notes-redux/src/App.jsx
@@ -2,11 +2,36 @@ import NoteForm from './components/NoteForm';
 import Notes from './components/Notes';
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+
   return (
-    <>
+    <div>
       <NoteForm />
+
+      <div>
+        all{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('ALL')}
+        />
+        important{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('IMPORTANT')}
+        />
+        nonimportant{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')}
+        />
+      </div>
       <Notes />
-    </>
+    </div>
   );
 };
 
