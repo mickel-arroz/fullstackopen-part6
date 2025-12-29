f671be066994b520516827bf54854fa65f8e9895
Mickel Arroz <arrozmickel@gmail.com>
2025-12-29 18:41:04 -0400

    6B Reducers combinados

--- notes-redux/src/main.jsx
+++ notes-redux/src/main.jsx
@@ -1,15 +1,37 @@
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore as createStore } from 'redux';
 
+import { legacy_createStore as createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-
 import App from './App';
-import noteReducer from './reducers/noteReducer';
 
-const store = createStore(noteReducer);
+import noteReducer, { createNote } from './reducers/noteReducer';
 
+import filterReducer, { filterChange } from './reducers/filterReducer';
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(filterChange('IMPORTANT'));
+store.dispatch(
+  createNote('combineReducers forms one reducer from many simple reducers')
+);
+
+console.log(store.getState());
+
+/*
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
+  </Provider>
+)*/
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <div />
   </Provider>
 );
