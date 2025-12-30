d97dca7424a0de2e26eb30ada0c22a64d93cf406
Mickel Arroz <arrozmickel@gmail.com>
2025-12-29 20:58:10 -0400

    6B Redux Toolkit y refactorizando la configuración del Store

--- notes-redux/src/main.jsx
+++ notes-redux/src/main.jsx
@@ -1,27 +1,8 @@
 import ReactDOM from 'react-dom/client';
-
-import { legacy_createStore as createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+
 import App from './App';
-
-import noteReducer, { createNote } from './reducers/noteReducer';
-
-import filterReducer, { filterChange } from './reducers/filterReducer';
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
-});
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-store.dispatch(filterChange('IMPORTANT'));
-store.dispatch(
-  createNote('combineReducers forms one reducer from many simple reducers')
-);
-
-console.log(store.getState());
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
