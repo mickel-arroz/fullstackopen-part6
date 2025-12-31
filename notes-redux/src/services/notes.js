20b1f204c735cb7faf87c98f6412ed3c009e6edb
Mickel Arroz <arrozmickel@gmail.com>
2025-12-31 17:53:38 -0400

    6C Enviando datos al backend

--- notes-redux/src/services/notes.js
+++ notes-redux/src/services/notes.js
@@ -10,4 +10,20 @@ const getAll = async () => {
   return await response.json();
 };
 
-export default { getAll };
+const createNew = async (content) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content, important: false }),
+  };
+
+  const response = await fetch(baseUrl, options);
+
+  if (!response.ok) {
+    throw new Error('Failed to create note');
+  }
+
+  return await response.json();
+};
+
+export default { getAll, createNew };
